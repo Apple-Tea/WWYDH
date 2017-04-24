8e322117a249259b32ff77dbe723c135f17d1ef4
FrenchLadyNerd <tgirodie7@gmail.com>
2017-04-24 12:34:19 -0400

    Update AddLocation.php
    Trying to go with Google's material design elements, Add Location button should ripple when clicked but not sure yet.

--- AddLocation.php
+++ AddLocation.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-blue.min.css" />
+	<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Bootstrap 101 Template</title>
 
@@ -33,15 +36,15 @@
         .panel .panel-body{
             
         }
-      *[role="form"] {
+      *[role=form] {
     max-width: 530px;
     padding: 25px;
     margin: 0 auto;
-    background-color: #fff;
+    background-color: #d4e2f9;
     border-radius: 0.3em;
 }
 
-*[role="form"] h2 {
+*[role=form] h2 {
      text-align: center;
     font-family: 'Anton', sans-serif;
 }
@@ -99,46 +102,121 @@
         
   
         
+      .auto-style1 {
+		text-align: center;
+	}
+        
+  
+        
+      .auto-style2 {
+		margin-left: 6px;
+	}
+        
+  
+        
+      .auto-style3 {
+	color: #525456;
+}
+        
+  
+        
       </style>
    <link type="text/css" rel="stylesheet" href="css/navigation.css">
     
      
   </head>
   <body>
+  <div class="auto-style1">
   <?php include_once("navigation.php");?>
       
 
 <div class="container">
     <form class="form-horizontal" role="form" method="post" enctype="multipart/form-data" action="">
-                <h2 class="text-center">Add Location</h2>
+                <h2 class="auto-style3">Add Location</h2>
                 
-                <div class="form-group">
-                    <label for="firstName" class="col-sm-3 control-label">Address</label>
+                <hr>
+                
+                <div class="form-group" style="height: 43px">
+                    <span class="auto-style3">
+                    <label for="firstName" class="col-sm-3 control-label">Address</label></span>
                     <div class="col-sm-9">
-                        <input type="text" name="firstName" placeholder="Address" class="form-control" autofocus>
+                        <input type="text" name="Address" placeholder="Address" class="form-control" autofocus>
                     </div>
                 </div>
                 
-                <div class="form-group">
-                    <label for="lastName" class="col-sm-3 control-label">City</label>
+                <div class="form-group" style="height: 43px">
+                    <span class="auto-style3">
+                    <label for="lastName" class="col-sm-3 control-label">City</label></span>
                     <div class="col-sm-9">
-                        <input type="text" name="lastName" placeholder="city o" class="form-control" autofocus>
-                    </div>
-                </div>
-                
-                
-                <div class="form-group">
-                    <label for="college" class="col-sm-3 control-label">State</label>
-                    <div class="col-sm-9">
-                        <input type="text" name="state" placeholder="State" class="form-control" autofocus>
+                        <input type="text" name="City" placeholder="city o" class="form-control" autofocus>
                     </div>
                 </div>
                 
                 
                 <div class="form-group">
-                    <label for="employment" class="col-sm-3 control-label">Zip</label>
+                    <span class="auto-style3">
+                    <label for="college" class="col-sm-3 control-label">State</label></span>
                     <div class="col-sm-9">
-                        <input type="text" name="zip" placeholder="zip" class="form-control" autofocus>
+                        <select class="auto-style2" name="State" style="width: 128px">
+						<option>Alabama</option>
+						<option>Alaska</option>
+						<option>Arizona</option>
+						<option>Arkansas</option>
+						<option>California</option>
+						<option>Colorado</option>
+						<option>Connecticut</option>
+						<option>Delaware</option>
+						<option>Florida</option>
+						<option>Georgia</option>
+						<option>Hawaii</option>
+						<option>Idaho</option>
+						<option>Indiana</option>
+						<option>Iowa</option>
+						<option>Kansas</option>
+						<option>Kentucky</option>
+						<option>Louisiana</option>
+						<option>Maine</option>
+						<option selected="">Maryland</option>
+						<option>Massachusetts</option>
+						<option>Michigan</option>
+						<option>Minnesota</option>
+						<option>Mississippi</option>
+						<option>Missouri</option>
+						<option>Montana</option>
+						<option>Nebraska</option>
+						<option>Nevada</option>
+						<option>New Hampshire</option>
+						<option>New Jersey</option>
+						<option>New Mexico</option>
+						<option>New York</option>
+						<option>North Carolina</option>
+						<option>North Dakota</option>
+						<option>Ohio</option>
+						<option>Oklahoma</option>
+						<option>Oregon</option>
+						<option>Pennsylvania</option>
+						<option>Rhode Island</option>
+						<option>South Carolina</option>
+						<option>South Dakota</option>
+						<option>Tennessee</option>
+						<option>Texas</option>
+						<option>Utah</option>
+						<option>Vermont</option>
+						<option>Virginia</option>
+						<option>Washington</option>
+						<option>West Virginia</option>
+						<option>Wisconsin</option>
+						<option>Wyoming</option>
+						</select>
+                    </div>
+                </div>
+                
+                
+                <div class="form-group">
+                    <span class="auto-style3">
+                    <label for="employment" class="col-sm-3 control-label">Zip</label></span>
+                    <div class="col-sm-9">
+                        <input type="text" name="Zip" placeholder="zip" class="form-control" autofocus>
                     </div>
                 </div>
                 
@@ -150,8 +228,10 @@
                                         <div class="col-md-2">
                     </div>
                     <div class="col-sm-4 col-sm-offset-3">
-                        <button type="submit" class="btn ">Add</button>
-                    </div>
+					<!-- Accent-colored raised button with ripple -->
+					<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+					  Button
+					</button>                    </div>
                 </div>
             </form> <!-- /form -->
 </div>
@@ -166,5 +246,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+  </div>
   </body>
 </html>
