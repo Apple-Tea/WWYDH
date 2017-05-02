dac4363ae22614ec7bcb6bd3c017412fa6344c7c
FrenchLadyNerd <tgirodie7@gmail.com>
2017-05-02 15:44:01 -0400

    Just a UI update
    I'm trying to change the button to a material design look... will take a bit more time.
    
    Also trying to move away from green XD

--- registerAndLogin.php
+++ registerAndLogin.php
@@ -19,20 +19,40 @@ if($user_ok == true){
     <!--Logo font-->
     <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
      <link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+
      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="html5shiv.min.js"></script>
+      <script src="respond.min.js"></script>
     <![endif]-->
  <style>    
  @import url(https://fonts.googleapis.com/css?family=Oswald:400,300);
  @import url(https://fonts.googleapis.com/css?family=Open+Sans);
-</style>  
+ <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+ import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+ .auto-style1 {
+	 text-align: center;
+ }
+ .auto-style1 {
+	 text-align: center;
+ }
+ .auto-style3 {
+	 text-align: center;
+	 font-weight: normal;
+ }
+ .auto-style4 {
+	 color: #1B758F;
+	 font-family: sans-serif;
+ }
+ </style>  
      <script src="js/ajax.js"></script>
 <script src="js/validationForSignInAndUp.js"></script>    
     
-<script src="http://code.jquery.com/jquery-1.11.1.js">
+<script src="jquery-1.11.1.js">
 </script>
     
     <!--jquery for changing login to register-->
@@ -58,30 +78,34 @@ if($user_ok == true){
 </script>   
  
 </head>
-<body>
+<body style="background-color: #C9DAE4">
 <div class="container">
    <div class="row">
     <div class="col-md-6 col-md-offset-3">
       <div class="panel panel-login">
-        <div class="panel-body">
+        <div class="auto-style1">
           <div class="row">
-            <div class="col-lg-12">
-              <form id="login-form" action="#" method="post" role="form" style="display: block;"  onsubmit="return false;">
-                  <h1 class="text-center"  style="font-family: 'Bungee', cursive;font-size:25px;">WWYDH</h1>
+          <div>
+          
+            <div class="col-lg-12">&nbsp;<form id="login-form" action="#" method="post" role="form" style="display: block;"  onsubmit="return false;">
+                  <h1 class="auto-style1"  style="font-family: 'Roboto', sans-serif;font-size:25px;">WWYDH</h1>
                   
-                <h2>LOGIN</h2>
+                <h2 class="auto-style3"style="font-family: 'Roboto', sans-serif;font-size:25px;">LOGIN</h2>
                  
                   
-                  <div class="form-group">
+                  <div class="auto-style1">
                     <input type="text" name="logusername"  tabindex="1" class="form-control" placeholder="Username"
                            id="luserName" autocomplete="off">
                    
                   </div>
-                  <div class="form-group">
+                  <div class="auto-style1">
                     <input type="password" name="password" id="LogInpassword" tabindex="2" class="form-control" placeholder="Password">
                   </div>
+                  
+                  <div>
+                  <FlatButton label="Log In" /></div>
                 
-                  <div class="col-sm-6 form-group ">     
+                  <div class="auto-style1">     
                         <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In" onclick="login()">
                       <span id="LogInStatus"></span>
                       
@@ -90,7 +114,8 @@ if($user_ok == true){
 										<div class="row">
 											<div class="col-lg-12">
 												<div class="text-center">
-													<a href="pasword-recovery.php" tabindex="5" class="forgot-password">Forgot Password?</a>
+													<a href="pasword-recovery.php" tabindex="5" class="forgot-password">
+													<span class="auto-style4">Forgot Password?</span></a>
 												</div>
 											</div>
 										</div>
@@ -99,24 +124,24 @@ if($user_ok == true){
               <form id="register-form" method="post" role="form" style="display: none;"  onsubmit="return false;">
                   <h1 class="text-center"  style="font-family: 'Bungee', cursive;font-size:25px;">WWYDH</h1>
                 <h2>REGISTER</h2>
-                  <div class="form-group">
+                  <div class="auto-style1">
                     <input type="text" name="username" onkeyup="check('userName','php/userNameCheck.php','unamestatus','username=')" onblur="isEmpty('userName','unamestatus')" id="userName" tabindex="1" class="form-control" placeholder="Username" value="">
                        <span id="unamestatus"></span>
                   </div>
                   
-                  <div class="form-group">
+                  <div class="auto-style1">
                     <input type="email" name="email" id="regEmail" tabindex="1" class="form-control" onblur=" checkEmail()" placeholder="Email Address" onkeyup="isEmpty('regEmail','emailStatus')">
                       <span id="emailStatus"></span>
                   </div>
                   
-                  <div class="form-group">
+                  <div class="auto-style1">
                     <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" onKeyup="checkPasswordStrength()" onblur="isEmpty('password','passwordStatus')">
                       <span id="passwordStatus"></span>
                   </div>
                   
                   
                   
-                  <div class="form-group">
+                  <div class="auto-style1">
                     <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password" onkeyup="confirmPass()" onblur="isEmpty('confirm-password','passStatus')">
                     <span id="passStatus"></span> 
                       
@@ -125,7 +150,7 @@ if($user_ok == true){
                   
                   <div class="form-group">
                     <div class="row">
-                      <div class="col-sm-6 col-sm-offset-3">  
+                      <div class="auto-style1">  
                         
                           <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" onclick="signup()" value="Register Now" onblur="isEmpty('register-submit','status')">
                           
@@ -138,13 +163,11 @@ if($user_ok == true){
             </div>
           </div>
         </div>
-        <div class="panel-heading">
+        <div class="auto-style1">
           <div class="row">
+                        </div>
             <div class="col-xs-6 tabs">
-              <a href="#" class="active" id="login-form-link"><div class="login">LOGIN</div></a>
-            </div>
-            <div class="col-xs-6 tabs">
-              <a href="#" id="register-form-link" ><div class="register">REGISTER</div></a>
+              <a href="#" id="register-form-link" ><div class="auto-style4">REGISTER</div></a>
             </div>
           </div>
         </div>
@@ -153,7 +176,7 @@ if($user_ok == true){
   </div>
 </div>
  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
 </body>
